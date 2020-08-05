f28657c8d98cb6176d64922c2c7fc68df12cc8f4
Abhinav <es15btech11002@iith.ac.in>
2020-08-05 18:42:25 +0900

    Implemented parsing twitter dataset

--- gcn.py
+++ gcn.py
@@ -7,11 +7,10 @@ from nltk.tree import Tree
 
 class GCN:
 
-  def __init__(self, file_name):
-    self.file_name = file_name 
+  def __init__(self):
     self.en_nlp = spacy.load("en")
   
-  def parse(self):
+  def parse_sem_eval(self):
     tree = ET.parse(self.file_name)
     root = tree.getroot() 
     parsed_data = []   
@@ -20,6 +19,19 @@ class GCN:
         for sentence in sentences:
           parsed_data.append((sentence.find('text').text))
     return parsed_data 
+
+  def parse_twitter(self):
+    parsed_data = []
+    
+    count = 0
+    with open(self.file_name, "r") as file1:
+      for line in file1:
+        stripped_line = line.strip()
+        if count%3 == 0:
+          parsed_data.append(stripped_line)
+        count += 1
+    return parsed_data        
+    
 
   def nltk_spacy_tree(self, sent):
       """
@@ -42,12 +54,19 @@ class GCN:
       # The first item in the list is the full tree
       tree[0].draw()
    
-  def run(self):
-    data = self.parse()
+  def run_sem_eval(self, file_name):
+    self.file_name = file_name
+    data = self.parse_sem_eval()
     for text in data:
       self.nltk_spacy_tree(text)
 
+  def run_twitter(self, file_name):
+    self.file_name = file_name
+    data = self.parse_twitter()
+    for text in data:
+      self.nltk_spacy_tree(text)
+    
 
-gcn = GCN('SemEval16_gold_Laptops/EN_LAPT_SB1_TEST_.xml.gold')
-gcn.run() 
-
+gcn = GCN()
+#gcn.run_sem_eval('SemEval16_gold_Laptops/EN_LAPT_SB1_TEST_.xml.gold') 
+gcn.run_twitter('Twitter-acl-14-short-data/train.txt')
